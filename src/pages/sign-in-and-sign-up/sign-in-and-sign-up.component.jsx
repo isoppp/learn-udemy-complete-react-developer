8708c81185f8efdfc67ff99fbd3815ffd5f4ea17
isoppp <prog@d2deck.com>
2019-09-25 15:07:25 -0700

    add sign-in and form components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+  <div>
+    <div className="sign-in-and-sign-up">SIGN IN</div>
+    <SignIn/>
+  </div>
+)
+
+export default SignInAndSignUpPage
